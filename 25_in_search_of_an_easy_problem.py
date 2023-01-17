87e6a82231e6e11f428f94bc27f107f373be8aaf
Rishil Ramesh <rishil.ramesh@outlook.com>
2023-01-17 16:12:03 +0530

    Add solution to 'In search of an easy problem' problem

---
+++ 25_in_search_of_an_easy_problem.py
@@ -0,0 +1,15 @@
+# In Search of an easy problem
+# https://codeforces.com/problemset/problem/1030/A
+
+def main():
+    number_of_people = int(input())
+    people_opinions = input()
+
+    if "1" in people_opinions:
+        print("HARD")
+    else:
+        print("EASY")
+
+
+if __name__ == "__main__":
+    main()
